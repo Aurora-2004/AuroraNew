d63d04f942eb3a77b165246cb0ae3d329e492546
silent ben8x <134549461+Aurora-2004@users.noreply.github.com>
2026-09-02 15:31:25 +0100

    Create TreeClassPrices.lua

---
+++ TreeClassPrices.lua
@@ -0,0 +1,144 @@
+
+return {
+
+    ["Generic"] = {
+
+        Log   = 1.5,
+        Plank = 10,
+
+    };
+
+    ["Oak"] = {
+
+        Log   = 0.75,
+        Plank = 6,
+
+    };
+
+    ["Cherry"] = {
+
+        Log   = 1.3,
+        Plank = 10.5,
+
+    };
+
+    ["BlueSpruce"] = {
+
+        Log   = 20,
+        Plank = 40,
+
+    };
+
+    ["Birch"] = {
+
+        Log   = 2.25,
+        Plank = 15,
+
+    };
+
+    ["Volcano"] = {
+
+        Log   = 3.5,
+        Plank = 28,
+
+    };
+
+    ["Fir"] = {
+
+        Log   = 3.2,
+        Plank = 18,
+
+    };
+
+    ["Pine"] = {
+
+        Log   = 3.2,
+        Plank = 18,
+
+    };
+
+    ["Frost"] = {
+        
+        Log   = 9,
+        Plank = 106,
+
+    };
+
+    ["LoneCave"] = {
+
+        Log   = 150,
+        Plank = 420,
+
+    };
+
+    ["Walnut"] = {
+
+        Log   = 1.2,
+        Plank = 11,
+
+    };
+
+    ["GreenSwampy"] = {
+
+        Log   = 4.4,
+        Plank = 30,
+
+    };
+
+    ["GoldSwampy"] = {
+
+        Log   = 5.7,
+        Plank = 36,
+
+    };
+
+    ["SnowGlow"] = {
+
+        Log   = 1.5,
+        Plank = 10,
+
+    };
+
+    ["Palm"] = {
+
+        Log   = 2.5,
+        Plank = 32,
+
+    };
+
+    ["Koa"] = {
+
+        Log   = 2.8,
+        Plank = 26.4,
+
+    };
+
+    ["CaveCrawler"] = {
+
+        Log   = 8,
+        Plank = 35,
+
+    };
+
+    ["Spooky"] = {
+
+        Log   = 19,
+        Plank = 54,
+
+    };
+
+    ["SpookyNeon"] = {
+
+        Log   = 25,
+        Plank = 90,
+
+    };
+
+    ["Sign"] = {
+
+        Log   = 170,
+        Plank = 170,
+
+    };
+
+};
